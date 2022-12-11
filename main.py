333aac0c8d80c3a6c5a3ab60612e103e0db5fbde
Rahman Aziz <rahmanazizf@gmail.com>
2022-12-11 17:32:22 +0700

    start over

---
+++ main.py
@@ -0,0 +1,13 @@
+# import libraries yang diperlukan termasuk modul untuk inisiasi koneksi ke database
+import mysql.connector
+from tabulate import tabulate
+
+# definisikan variabel yang digunakan untuk membangun koneksi ke server
+
+# buat koneksi ke database
+
+
+# daftar fungsi-fungsi yang digunakan
+
+
+# menu
