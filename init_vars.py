7ba1930753c9f182eb8a3e500f9038261a2ab571
Rahman Aziz <rahmanazizf@gmail.com>
2022-12-11 17:38:24 +0700

    variables that will be used to establish a connection to database

---
+++ init_vars.py
@@ -0,0 +1,9 @@
+"""
+Modul ini berisi variabel-variabel untuk membangun koneksi ke database.
+"""
+
+# inisiasi variabel host, password, dll
+host = "localhost"
+user = "root"
+password = "lmsserderhana1112"
+db_name = "inventori_buku"
