20a771cc7376a3efa8a11b650d40d7d38a81de36
Rahman Aziz <rahmanazizf@gmail.com>
2022-12-11 17:47:14 +0700

    module contained functions to create new database in server

---
+++ create_db.py
@@ -0,0 +1,12 @@
+"""
+Modul ini memuat functions yang digunakan untuk membuat database
+"""
+
+# import libraries yang diperlukan
+
+# definisikan variables dari modul init_vars.py
+
+# fungsi untuk membuat tabel di database
+# gunakan handling exceptions
+
+# insert preceding data to database
